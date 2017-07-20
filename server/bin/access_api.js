f0049e72922aa18cd91b4cc67258718e7a5dbe8b
JuiMin <d95wang@uw.edu>
2017-07-19 20:30:20 -0700

    progress on the api key

--- server/bin/access_api.js
+++ server/bin/access_api.js
@@ -2,3 +2,12 @@
 var KEY = RGAPI-3dcb37b4-c1b9-4c8d-af30-8223d9256298;
 
 // XHTTP
+var request = new XMLHttpRequest();
+
+request.open('GET', 'https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/JuiMIn');
+
+// Set the request header's token to be the API key
+request.setRequestHeader('X-Riot-Token', KEY);
+
+// Send the HTTP request
+request.send();
