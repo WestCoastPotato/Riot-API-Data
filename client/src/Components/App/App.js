5e6e70e57b80e0e5974a16011b22f3c08dd21ce0
JuiMin <d95wang@gmail.com>
2017-07-28 11:01:16 -0700

    Routing is set up in the Application. Use home for the search bar and the other components for the other stuff

--- client/src/Components/App/App.js
+++ client/src/Components/App/App.js
@@ -2,19 +2,34 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Import routes
+import Home from '../Home/Home.js'
+
+// React Router Dom import components
+// using ES6 modules
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>RIOT API Project v0.0.1</h2>
+      <BrowserRouter>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>API Project v0.0.2</h2>
+          </div>
+          
+          <div id="menu">
+            <Link to="/">Home</Link>
+          </div>
+
+          <p className="App-intro">
+            This Project will aim to gather data about a specific user from the Riot API and provide meaningful
+            analytics surrounding their performance.
+          </p>
+          <Route exact path="/" component={Home}/>
         </div>
-        <p className="App-intro">
-          This Project will aim to gather data about a specific user from the Riot API and provide meaningful
-          analytics surrounding their performance.
-        </p>
-      </div>
+      </BrowserRouter>
     );
   }
 }
