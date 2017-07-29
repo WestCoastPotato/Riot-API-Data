94847805a06d5a1e60a60f68dcf0f7d24ee18bb7
JuiMin <d95wang@uw.edu>
2017-07-29 10:13:41 -0700

    Fix routing parameters for the user page. When nick finishes the Home page, the user should be redirected to the user bar after the search is complete

--- client/src/Components/App/App.js
+++ client/src/Components/App/App.js
@@ -3,13 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 // Import routes
-import Home from '../Home/Home.js'
+import Home from '../Home/Home.js';
+import User from '../User/User.js';
 
 // React Router Dom import components
 // using ES6 modules
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 class App extends Component {
+
   render() {
     return (
       <BrowserRouter>
@@ -18,16 +20,19 @@ class App extends Component {
             <img src={logo} className="App-logo" alt="logo" />
             <h2>API Project v0.0.2</h2>
           </div>
-          
+
           <div id="menu">
             <Link to="/">Home</Link>
+            <Link to="/user">User</Link>
           </div>
 
           <p className="App-intro">
             This Project will aim to gather data about a specific user from the Riot API and provide meaningful
             analytics surrounding their performance.
           </p>
+          
           <Route exact path="/" component={Home}/>
+          <Route path="/user/:userID" component={User}></Route>
         </div>
       </BrowserRouter>
     );
--- client/src/commonAssets/README.md
+++
@@ -1,4 +0,0 @@
-This should contain the common assets like images and the like. This could go away thoguh since we could store stuff elsewhere
-
-
-
